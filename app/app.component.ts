213413f2501f7246edf2749f27b1c9e73f6411f2
James Higgins <james.milan.higgins@gmail.com>
2017-06-09 08:45:47 -0700

    add new component  files, pipe file, and animal model

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+// import { Keg } from './keg.model';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="header">
+     <h1>TEST ROUTE PAGE</h1>
+  </div>
+
+
+  `
+})
+
+export class AppComponent {
+  
+}
