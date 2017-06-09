a453832bf5ebd42e0ecce6a117bad78c7236bcca
James Higgins <james.milan.higgins@gmail.com>
2017-06-09 09:22:47 -0700

    add dynamic animal and get it to display on app component route

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,14 +4,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="header">
-     <h1>TEST ROUTE PAGE</h1>
+   <h1>Portland Zoo</h1>
+   <div *ngFor="let animal of animals">
+    <h2> Animal Details</h2>
+    <p>{{animal.species}}</p>
+    <p>{{animal.name}}</p>
+    <p>{{animal.age}}</p>
+    <p>{{animal.diet}}</p>
+    <p>{{animal.location}}</p>
+    <p>{{animal.caretakers}}</p>
+    <p>{{animal.sex}}</p>
+    <p>{{animal.like}}</p>
+    <p>{{animal.dislike}}</p>
   </div>
-
-
   `
 })
 
 export class AppComponent {
-  
+  animals: Animal[] = [
+    new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'cool shade', 'loud noises')
+  ];
+
+}
+
+export class Animal {
+  constructor (public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public like: string, public dislike: string) {}
 }
